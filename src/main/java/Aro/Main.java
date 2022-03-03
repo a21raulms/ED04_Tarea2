76fee0650dcc9b2e6d6b5f0fd97aece8d4aeb52c
a21raulms <a21raulms@iessanclemente.net>
2022-03-03 10:25:43 +0100

    Renombramos la clase Circulo por Aro

---
+++ src/main/java/Aro/Main.java
@@ -0,0 +1,29 @@
+package Aro;
+
+import java.text.DecimalFormat;
+
+/**
+ * Pruebas de refactorización en NetBeans con la clase Aro
+ * @author a21raulms
+ */
+public class Main {
+    public static void main(String[] args) {
+        Aro Aro = new Aro(37,43,2.5);
+        String saida =
+                "A coordenada X é "+Aro.obterX()+
+                "\nA coordenada Y é "+Aro.obterY()+
+                "\nO radio é "+Aro.obterRadio();
+        Aro.establecerX(35);
+        Aro.establecerY(20);
+        Aro.establecerRadio(4.2);
+        DecimalFormat dousDixitos = new DecimalFormat("0.00");
+        saida+="\nO diámetro é "+dousDixitos.format(Aro.obterDiametro());
+        saida+="\nA circunferencia é "+dousDixitos.format(Aro.obterCircunferencia());
+        saida+="\nA área é "+dousDixitos.format(Aro.obterArea());
+        System.out.println(saida);
+        
+        System.out.println(saida);
+        System.exit(0);
+    }
+
+}
