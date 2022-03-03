4e862912d927adf688a80c6c2f79e1235333c21f
a21raulms <a21raulms@iessanclemente.net>
2022-03-03 11:41:20 +0100

    Introducimos la constante LIMITERADIO

--- src/main/java/Aro/Aro.java
+++ src/main/java/Aro/Aro.java
@@ -5,7 +5,9 @@ package Aro;
  * @author a21raulms
  */
 public class Aro {
-    public static final double MINIMO = 0.0;
+
+    public static final double LIMITERADIO = 0.0;
+    public static final double MINIMO = LIMITERADIO;
 
     private int coordenadaX;
     private int coordenadaY;
