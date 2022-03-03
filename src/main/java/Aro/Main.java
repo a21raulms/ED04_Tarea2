f3e498a1463a6e42af1414af008857c78b07e1ef
a21raulms <a21raulms@iessanclemente.net>
2022-03-03 13:21:41 +0100

    sustitución de metodos por get y set

--- src/main/java/Aro/Main.java
+++ src/main/java/Aro/Main.java
@@ -10,12 +10,12 @@ public class Main {
     public static void main(String[] args) {
         Aro Aro = new Aro(37,43,2.5);
         String saida =
-                "A coordenada X é "+Aro.obterX()+
-                "\nA coordenada Y é "+Aro.obterY()+
-                "\nO radio é "+Aro.obterRadio();
-        Aro.establecerX(35);
-        Aro.establecerY(20);
-        Aro.establecerRadio(4.2);
+                "A coordenada X é "+Aro.getCoordenadaX()+
+                "\nA coordenada Y é "+Aro.getCoordenadaY()+
+                "\nO radio é "+Aro.getRadio();
+        Aro.setCoordenadaX(35);
+        Aro.setCoordenadaY(20);
+        Aro.setRadio(4.2);
         DecimalFormat dousDixitos = new DecimalFormat("0.00");
         saida+="\nO diámetro é "+dousDixitos.format(Aro.obterDiametro());
         saida+="\nA circunferencia é "+dousDixitos.format(Aro.obterCircunferencia());
--- src/test/java/Aro/AroTest.java
+++ src/test/java/Aro/AroTest.java
@@ -20,82 +20,12 @@ public class AroTest {
     public AroTest() {
     }
     
-    /**
-     * Test of establecerX method, of class Aro.
-     */
-    @Test
-    public void testEstablecerX() {
-        System.out.println("establecerX");
-        int valorX = 0;
-        Aro instance = new Aro();
-        instance.establecerX(valorX);
-    }
 
-    /**
-     * Test of obterX method, of class Aro.
-     */
-    @Test
-    public void testObterX() {
-        System.out.println("obterX");
-        Aro instance = new Aro(0,0,0.1);
-        int expResult = 0;
-        int result = instance.obterX();
-        assertEquals(expResult, result);
-    }
 
-    /**
-     * Test of establecerY method, of class Aro.
-     */
-    @Test
-    public void testEstablecerY() {
-        System.out.println("establecerY");
-        int valorY = 0;
-        Aro instance = new Aro();
-        instance.establecerY(valorY);
-    }
 
-    /**
-     * Test of obterY method, of class Aro.
-     */
-    @Test
-    public void testObterY() {
-        System.out.println("obterY");
-        Aro instance = new Aro(0,0,0.1);
-        int expResult = 0;
-        int result = instance.obterY();
-        assertEquals(expResult, result);
-    }
 
-    /**
-     * Test of establecerRadio method, of class Aro.
-     */
-    @Test
-    public void testEstablecerRadio() {
-        System.out.println("establecerRadio");
-        Aro instance = new Aro();
-        instance.establecerRadio(0.0);
-        instance.establecerRadio(0.1);
-        instance.establecerRadio(-0.1);
-    }
 
-    /**
-     * Test of obterRadio method, of class Aro.
-     */
-    @Test
-    public void testObterRadio() {
-        System.out.println("obterRadio");
-        Aro instance = new Aro(0,0,0.1);
-        double result = instance.obterRadio();
-        assertEquals(0.1, result,0);
 
-        instance.establecerRadio(0.1);
-        result= instance.obterRadio();
-        assertEquals(0.1, result,0);
-
-        instance.establecerRadio(-0.1);
-        result= instance.obterRadio();
-        assertEquals(0.0, result,0);
-    }
 
     /**
      * Test of obterDiametro method, of class Aro.
@@ -139,11 +69,11 @@ public class AroTest {
     public void testTrasladarCentro() {
         System.out.println("trasladarCentro");
         Aro instance = new Aro();
-        int resultx=instance.obterX();
-        int resulty=instance.obterY();
+        int resultx=instance.getCoordenadaX();
+        int resulty=instance.getCoordenadaY();
         instance.trasladarCentro(5, 6);
-        int resultnx = instance.obterX();
-        int resultny = instance.obterY();
+        int resultnx = instance.getCoordenadaX();
+        int resultny = instance.getCoordenadaY();
         assertEquals(resultx+5, resultnx);
         assertEquals(resulty+6, resultny);
     }
